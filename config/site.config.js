dbc86d8244c169d5203f781d78b6d3f69aaa2749
nova1751 <97817985+nova1751@users.noreply.github.com>
2022-11-18 16:51:57 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -12,8 +12,6 @@ module.exports = {
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
   icon: '/icons/icon.png',
-  
-  favicon: '/icons/icon.png',
 
   // Prefix for KV Storage
   kvPrefix: process.env.KV_PREFIX || '',
