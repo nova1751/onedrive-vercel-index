5458541bc41bdbe22b9999fd1310a9e05a0e8dca
nova1751 <97817985+nova1751@users.noreply.github.com>
2023-02-20 20:54:37 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    '<object data="https://img.shields.io/badge/Source-Github-d021d6?style=flat&logo=GitHub&link=https://github.com/"></object><object data="https://img.shields.io/badge/Vercel-Hosted-blue?style=social&logo=Vercel&link=https://vercel.com/"></object>Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>.' ,
+    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>.<object data="https://img.shields.io/badge/Source-Github-d021d6?style=flat&logo=GitHub&link=https://github.com/"></object><object data="https://img.shields.io/badge/Vercel-Hosted-blue?style=social&logo=Vercel&link=https://vercel.com/"></object>' ,
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
